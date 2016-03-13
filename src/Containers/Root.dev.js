f3d86f1a01296507835c5ce41075a2a554efb891
Anton Semenov <anton@anton.local>
2016-03-13 13:38:45 +0500

    fix Root.dev

--- src/Containers/Root.dev.js
+++ src/Containers/Root.dev.js
@@ -6,7 +6,9 @@ export default class Root extends Component {
   render() {
     return(
       <Provider >
-        <DevTool />
+        <div>
+          <DevTool />
+        </div>
       </Provider>
     );
   }
