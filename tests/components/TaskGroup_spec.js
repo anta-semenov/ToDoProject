0a2ff7350129e02cd381ee0a99944d7797a14e10
maksimsemenov <maksa.semenov@gmail.com>
2016-03-24 21:42:16 -0400

    Test for TaskGroup component

--- tests/components/TaskGroup_spec.js
+++ tests/components/TaskGroup_spec.js
@@ -1,4 +1,4 @@
-import expect from 'chai'
+import { expect } from 'chai'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { renderIntoDocument, scryRenderedDOMComponentsWithClass } from 'react-addons-test-utils'
@@ -16,8 +16,17 @@ describe('TaskGroup component tests', () => {
       <TaskGroup groupName = {groupName} tasks = {[]} onTaskClick={taskClickHandler} onTaskCheckboxClick={taskCheckboxClickHandler} />
     )
     const groupTitles = scryRenderedDOMComponentsWithClass(taskComponent, groupClassName)
-
-    expect(groupTitles.lenght).to.equal(1)
+    expect(groupTitles.length).to.equal(1)
     expect(groupTitles[0].textContent).to.equal(groupName)
+  })
+  it('Should not render group name if it has not one', () => {
+    const groupClassName='group__name'
+    const taskClickHandler = () => {}
+    const taskCheckboxClickHandler = () => {}
+    const taskComponent = renderIntoDocument(
+      <TaskGroup tasks = {[]} onTaskClick={taskClickHandler} onTaskCheckboxClick={taskCheckboxClickHandler} />
+    )
+    const groupTitles = scryRenderedDOMComponentsWithClass(taskComponent, groupClassName)
+    expect(groupTitles.length).to.equal(0)
   })
 })
