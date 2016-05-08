18898d4b703d77f12399ebb41ed1eb4b9f4b3e1d
Anton Semenov <anton@anton.local>
2016-05-08 09:49:11 +0500

    Changed project reducer

--- src/reducer/project.js
+++ src/reducer/project.js
@@ -2,7 +2,7 @@ import { fromJS } from 'immutable'
 import { NEW_PROJECT_TITLE } from '../constants/defaults'
 import * as actionTypes from '../constants/actionTypes'
 
-export default function project(state = fromJS([]), action) {
+export default function project(state = fromJS({}), action) {
   switch (action.type) {
     case actionTypes.ADD_PROJECT:
       return addProject(state, action.properties)
@@ -22,34 +22,30 @@ export default function project(state = fromJS([]), action) {
 }
 
 function addProject(state, properties = {}) {
-  const newId = state.reduce((maxId, item) => {
-    return Math.max(maxId, item.get('id'))
-  }, -1) + 1
+  if (!properties.id || state.has(properties.id)) {
+    return state
+  }
   const newProject = fromJS({
-    id: newId,
+    id: properties.id,
     title: NEW_PROJECT_TITLE,
     completed: false
   })
-  return state.push(newProject.merge(properties))
+  return state.set(properties.id, newProject.merge(properties))
 }
 
 function removeProject(state, id) {
-  const index = state.findIndex(item => item.get('id') === id)
-  if (index > -1) {
-    return state.delete(index)
-  }
-  else {
-    return state
-  }
-
+  return state.delete(id)
 }
 
 function editProject(state, id, properties = {}) {
-  const index = state.findIndex(item => item.get('id') === id)
-  return state.mergeIn([index], properties)
+  if (properties.id && properties.id != id) {
+    const temp = state.get(id)
+    return state.delete(id).set(properties.id, temp).mergeIn([properties.id], properties)
+  } else {
+    return state.mergeIn([id], properties)
+  }
 }
 
 function completeProject(state, id, status = false) {
-  const index = state.findIndex(item => item.get('id') === id)
-  return state.setIn([index, 'completed'], status)
+  return state.setIn([id, 'completed'], status)
 }
