ed2b5d10c82e726e96c3ad52c5ac71d0427c63b9
Anton Semenov <anton@anton.local>
2016-05-10 21:41:32 +0500

    Task updater

--- src/backend/firebase/taskUpdater.js
+++ src/backend/firebase/taskUpdater.js
@@ -3,14 +3,109 @@ import * as actionTypes from '../../constants/actionTypes'
 export default function taskUpdater(updateObjects, action, newState) {
   switch (action.type) {
     case actionTypes.ADD_TASK:
-      return addTask(updateObjects, newState)
+      return addTask(updateObjects, action.properties, newState)
+    case actionTypes.REMOVE_TASK:
+      return removeTask(updateObjects, action.id)
+    case actionTypes.EDIT_TASK:
+      return editTask(updateObjects, action.id, action.properties, newState)
+    case actionTypes.COMPLETE_TASK:
+      return completeTask(updateObjects, action.id, action.status)
+    case actionTypes.SET_TASK_TODAY:
+      return setTaskToday(updateObjects, action.id, action.status)
+    case actionTypes.ADD_TASK_TO_PROJECT:
+      return addTaskToProject(updateObjects, action.id, action.project)
+    case actionTypes.ADD_TASK_CONTEXT:
+      return editTaskContext(updateObjects, action.id, newState)
+      case actionTypes.REMOVE_TASK_CONTEXT:
+        return editTaskContext(updateObjects, action.id, newState)
     default:
       return updateObjects
   }
 }
 
-function addTask(updateObjects, newState) {
-  const value = newState.last().toJS()
-  const updateURL = 'task/' + value.id
-  return updateObjects.push({updateURL, value})
+function addTask(updateObjects, properties, newState) {
+  if (properties.id) {
+    const value = newState.get(properties.id).toJS()
+    const updateURL = 'task/' + properties.id
+    updateObjects.push({updateURL, value})
+  }
+
+  return updateObjects
+}
+
+function removeTask(updateObjects, id) {
+  if (id) {
+    updateObjects.push({
+      updateURL: 'task/' + id,
+      value: null
+    })
+  }
+
+  return updateObjects
+}
+
+function editTask(updateObjects, id, properties, newState) {
+  if (properties.id && id != properties.id) {
+    updateObjects.push({
+      updateURL: 'task/' + id,
+      value: null
+    })
+    updateObjects.push({
+      updateURL: 'task/' + properties.id,
+      value: newState.get(properties.id).toJS()
+    })
+  } else {
+    Object.keys(properties).forEach(key => {
+      updateObjects.push({
+        updateURL: 'task/' + id + '/' + key,
+        value: newState.getIn([id, key])
+      })
+    })
+  }
+
+  return updateObjects
+}
+
+function completeTask(updateObjects, id, status) {
+  if (id) {
+    updateObjects.push({
+      updateURL: 'task/' + id + '/completed',
+      value: status
+    })
+  }
+
+  return updateObjects
+}
+
+function setTaskToday(updateObjects, id, status) {
+  if (id) {
+    updateObjects.push({
+      updateURL: 'task/' + id + '/today',
+      value: status
+    })
+  }
+
+  return updateObjects
+}
+
+function addTaskToProject(updateObjects, id, project) {
+  if (id) {
+    updateObjects.push({
+      updateURL: 'task/' + id + '/project',
+      value: (project || null)
+    })
+  }
+
+  return updateObjects
+}
+
+function editTaskContext(updateObjects, id, newState) {
+  if (id) {
+    updateObjects.push({
+      updateURL: 'task/' + id + '/contexts',
+      value: newState.getIn([id, 'contexts']) ? newState.getIn([id, 'contexts']).toJS() : null
+    })
+  }
+
+  return updateObjects
 }
---
+++ tests/firebase/taskUpdater_spec.js
@@ -0,0 +1,207 @@
+import { expect } from 'chai'
+import { fromJS, Set } from 'immutable'
+import taskUpdater from '../../src/backend/firebase/taskUpdater'
+import { PRIORITY_NONE, PRIORITY_MEDIUM } from '../../src/constants/priorityLevels'
+import taskReducer from '../../src/reducer/task'
+import * as actions from '../../src/actions/taskActions'
+
+describe('Firebase task updater', () => {
+  const testState = fromJS({
+      b41sogy3s0o1: {
+        id: 'b41sogy3s0o1',
+        title: 'test task',
+        completed: false,
+        today: false,
+        priority: PRIORITY_NONE,
+        project: 'a2ps56y3sfo0',
+        contexts: Set(['cb3sr7yd9fo0', 'cb3sr7yd9fo1'])
+      }
+    })
+  it('Should handle Add task action', () => {
+    const newTaskProperties = {
+      id: 'b41sogy3s0o2',
+      title: 'test task 2'
+    }
+    const action = actions.addTask(newTaskProperties)
+    const newState = taskReducer(testState, action)
+
+    const expectedUpdateObject = [{
+      updateURL: 'task/b41sogy3s0o2',
+      value: {
+        id: 'b41sogy3s0o2',
+        title: 'test task 2',
+        completed: false,
+        today: false,
+        priority: PRIORITY_NONE
+      }
+    }]
+    expect(taskUpdater([], action, newState)).to.deep.equal(expectedUpdateObject)
+  })
+
+  it('Should handle remove task action', () => {
+    const action = actions.removeTask('b41sogy3s0o1')
+    const newState = taskReducer(testState, action)
+
+    const expectedUpdateObject = [{
+      updateURL: 'task/b41sogy3s0o1',
+      value: null
+    }]
+
+    expect(taskUpdater([], action, newState)).to.deep.equal(expectedUpdateObject)
+  })
+
+  it('Should handle complete task action', () => {
+    const newCompletedStatus = true
+    const action = actions.completeTask('b41sogy3s0o1', newCompletedStatus)
+    const newState = taskReducer(testState, action)
+
+    const expectedUpdateObject = [{
+      updateURL: 'task/b41sogy3s0o1/completed',
+      value: newCompletedStatus
+    }]
+
+    expect(taskUpdater([], action, newState)).to.deep.equal(expectedUpdateObject)
+  })
+
+  it('Should handle set today task action', () => {
+    const newTodayStatus = true
+    const action = actions.setTaskToday('b41sogy3s0o1', newTodayStatus)
+    const newState = taskReducer(testState, action)
+
+    const expectedUpdateObject = [{
+      updateURL: 'task/b41sogy3s0o1/today',
+      value: newTodayStatus
+    }]
+
+    expect(taskUpdater([], action, newState)).to.deep.equal(expectedUpdateObject)
+  })
+
+  describe('Edit task', () => {
+    it('Should handle edit task action with multiple changes', () => {
+      const newTaskProperties = {
+        title: 'check task updater',
+        priority: PRIORITY_MEDIUM
+      }
+      const action = actions.editTask('b41sogy3s0o1', newTaskProperties)
+      const newState = taskReducer(testState, action)
+
+      const expectedUpdateObject = [
+        {
+          updateURL: 'task/b41sogy3s0o1/title',
+          value: 'check task updater'
+        },
+        {
+          updateURL: 'task/b41sogy3s0o1/priority',
+          value: PRIORITY_MEDIUM
+        }
+      ]
+
+      expect(taskUpdater([], action, newState)).to.deep.equal(expectedUpdateObject)
+    })
+
+    it('Should handle edit task action with id changes', () => {
+      const newTaskProperties = {
+        title: 'check task updater',
+        id: 'b41sogy3s0o2'
+      }
+      const action = actions.editTask('b41sogy3s0o1', newTaskProperties)
+      const newState = taskReducer(testState, action)
+
+      const expectedUpdateObject = [
+        {
+          updateURL: 'task/b41sogy3s0o1',
+          value: null
+        },
+        {
+          updateURL: 'task/b41sogy3s0o2',
+          value: {
+            id: 'b41sogy3s0o2',
+            title: 'check task updater',
+            completed: false,
+            today: false,
+            priority: PRIORITY_NONE,
+            project: 'a2ps56y3sfo0',
+            contexts: ['cb3sr7yd9fo0','cb3sr7yd9fo1']
+          }
+        }
+      ]
+
+      expect(taskUpdater([], action, newState)).to.deep.equal(expectedUpdateObject)
+    })
+  })
+
+  describe('Add task to project', () => {
+    it('Should handle add task to project action', () => {
+      const action = actions.addTaskToProject('b41sogy3s0o1', 'a2ps56y3sfo1')
+      const newState = taskReducer(testState, action)
+
+      const expectedUpdateObject = [{
+        updateURL: 'task/b41sogy3s0o1/project',
+        value: 'a2ps56y3sfo1'
+      }]
+
+      expect(taskUpdater([], action, newState)).to.deep.equal(expectedUpdateObject)
+    })
+
+    it('Should handle remove task from project action', () => {
+      const action = actions.addTaskToProject('b41sogy3s0o1', undefined)
+      const newState = taskReducer(testState, action)
+
+      const expectedUpdateObject = [{
+        updateURL: 'task/b41sogy3s0o1/project',
+        value: null
+      }]
+
+      expect(taskUpdater([], action, newState)).to.deep.equal(expectedUpdateObject)
+    })
+  })
+
+  describe('Context', () => {
+    it('Should handle add task context action', () => {
+      const action = actions.addTaskContext('b41sogy3s0o1', 'cb3sr7yd9fo3')
+      const newState = taskReducer(testState, action)
+
+      const expectedUpdateObject = [{
+        updateURL: 'task/b41sogy3s0o1/contexts',
+        value: ['cb3sr7yd9fo0', 'cb3sr7yd9fo1', 'cb3sr7yd9fo3']
+      }]
+
+      expect(taskUpdater([], action, newState)).to.deep.equal(expectedUpdateObject)
+    })
+
+    it('Should handle remove task context action', () => {
+      const action = actions.removeTaskContext('b41sogy3s0o1', 'cb3sr7yd9fo0')
+      const newState = taskReducer(testState, action)
+
+      const expectedUpdateObject = [{
+        updateURL: 'task/b41sogy3s0o1/contexts',
+        value: ['cb3sr7yd9fo1']
+      }]
+
+      expect(taskUpdater([], action, newState)).to.deep.equal(expectedUpdateObject)
+    })
+
+    it('Should handle remove task context action when no contexts will be', () => {
+      const state = fromJS({
+          b41sogy3s0o1: {
+            id: 'b41sogy3s0o1',
+            title: 'test task',
+            completed: false,
+            today: false,
+            priority: PRIORITY_NONE,
+            project: 'a2ps56y3sfo0',
+            contexts: Set(['cb3sr7yd9fo0'])
+          }
+        })
+      const action = actions.removeTaskContext('b41sogy3s0o1', 'cb3sr7yd9fo0')
+      const newState = taskReducer(state, action)
+
+      const expectedUpdateObject = [{
+        updateURL: 'task/b41sogy3s0o1/contexts',
+        value: null
+      }]
+
+      expect(taskUpdater([], action, newState)).to.deep.equal(expectedUpdateObject)
+    })
+  })
+})
