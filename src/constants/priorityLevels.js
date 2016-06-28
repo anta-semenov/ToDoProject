81ae57b72b95e861443934df5cb9b78839b0db4d
maksimsemenov <maksa.semenov@gmail.com>
2016-06-28 13:59:21 -0400

    Add priority rearrange

--- src/constants/priorityLevels.js
+++ src/constants/priorityLevels.js
@@ -3,3 +3,4 @@ export const PRIORITY_LOW = 'low'
 export const PRIORITY_MEDIUM = 'medium'
 export const PRIORITY_HIGH = 'high'
 export const PRIORITY_MAX = 'max'
+export const PRIORITY = [PRIORITY_NONE, PRIORITY_LOW, PRIORITY_MEDIUM, PRIORITY_HIGH, PRIORITY_MAX]
