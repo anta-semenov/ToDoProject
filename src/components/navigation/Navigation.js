a870c349c0689cea297802487651cbe661fd5da1
Anton Semenov <anton@anton.local>
2016-07-16 21:29:07 +0500

    Trying add scroll

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -2,28 +2,50 @@ import React from 'react'
 import ImmutablePropTypes from 'react-immutable-proptypes'
 import './Navigation.less'
 import NavigationGroup from '../navigationGroup/NavigationGroup'
+import { DropTarget } from 'react-dnd'
+import { TASK } from '../../constants/dndTypes'
 
-export default class Navigation extends React.Component {
+const scrollTarget = {
+  canDrop: () => true
+}
+
+const collectUpScroll = (connect, monitor) => ({
+  connectUpScrollTarget: connect.dropTarget(),
+  isHoveringUp: monitor.isOver()
+})
+
+class Navigation extends React.Component {
   constructor(props) {
     super(props)
   }
 
+  componentDidUpdate() {
+    if (this.props.isHoveringUp && this._navScrollView.scrollTop > 0) {
+      this._navScrollView.scrollTop -= this.state.scrollDiff !== 0 ? this.state.scrollDiff : 10
+      this.setState({scrollDiff: (this.state.scrollDiff !== 0 ? this.state.scrollDiff : 10)*1.1})
+    }
+  }
+
   render() {
     return (
-      <ul className='nav'>
-        {this.props.groups.map((group, index) =>
-          <NavigationGroup
-            key={index} //We should pass unique identificator for array items
-            items={group.items}
-            title={group.title}
-            onItemClick={this.props.onItemClick}
-            type={group.type}
-            addNewTitle={group.addNewTitle}
-            addNew={this.props.addNew}
-            onStopEditing={this.props.onStopEditing}
-          />
-        )}
-      </ul>
+      <div className='navigation'>
+        {this.props.connectUpScrollTarget(<div className='nav-scroll' />)}
+        <ul className='nav' ref={(ref) => this._navScrollView = ref}>
+          {this.props.groups.map((group, index) =>
+            <NavigationGroup
+              key={index} //We should pass unique identificator for array items
+              items={group.items}
+              title={group.title}
+              onItemClick={this.props.onItemClick}
+              type={group.type}
+              addNewTitle={group.addNewTitle}
+              addNew={this.props.addNew}
+              onStopEditing={this.props.onStopEditing}
+            />
+          )}
+        </ul>
+        <div className='nav-scroll'/>
+      </div>
     )
   }
 }
@@ -50,3 +72,5 @@ Navigation.propTypes = {
   addNew: React.PropTypes.func,
   onStopEditing: React.PropTypes.func
 }
+
+export default DropTarget(TASK, scrollTarget, collectUpScroll)(Navigation)
