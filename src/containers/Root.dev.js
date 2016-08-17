0dc57c77f72040b4bbb9a0bc470e1cff95307d3b
maksimsemenov <maksa.semenov@gmail.com>
2016-08-17 15:14:43 -0400

    Update developer and production root components

--- src/containers/Root.dev.js
+++ src/containers/Root.dev.js
@@ -2,15 +2,19 @@ import React, {Component} from 'react'
 import { Provider } from 'react-redux'
 import { Router, Route, browserHistory } from 'react-router'
 import App from '../components/app/App'
+import DevTool  from './DevTool'
 import Perf from 'react-addons-perf'
 
 export default class Root extends Component {
   render() {
     return(
       <Provider store={this.props.store}>
-        <Router history={browserHistory}>
-          <Route path='/(:section)(/:task)' component={App} />
-        </Router>
+        <div>
+          <Router history={browserHistory}>
+            <Route path='/(:section)(/:task)' component={App} />
+          </Router>
+          <DevTool />          
+        </div>
       </Provider>
     )
   }
