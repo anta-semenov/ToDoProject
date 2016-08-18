3d6f3aabc231ded7c5db4fa5e834707bda25bec9
maksimsemenov <maksa.semenov@gmail.com>
2016-08-18 09:27:18 -0400

    Fix error

--- src/containers/Root.prod.js
+++ src/containers/Root.prod.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Provider } from 'react-redux'
-import { Router, Route, browserHistory } from 'redux'
+import { Router, Route, browserHistory } from 'react-router'
 import App from '../components/app/App'
 
 export default class Root extends Component {
