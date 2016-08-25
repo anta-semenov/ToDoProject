c4f9150f7de440172d1409288d3fd0fae91f2636
maksimsemenov <maksa.semenov@gmail.com>
2016-08-25 16:24:09 -0400

    Add images for empty states
    Add images for empty states for Inbox, Next, Someday, and Project

--- src/components/elements/emptyTaskList/EmptyTaskList.js
+++ src/components/elements/emptyTaskList/EmptyTaskList.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { INBOX, TODAY, NEXT, SOMEDAY, CONTEXT, PROJECT } from '../../../constants/sectionTypes'
-import InboxImage from './inboxImage/InboxImage'
+import InboxImage from '../inboxImage/InboxImage'
+import NextImage from '../nextImage/NextImage'
+import SomedayImage from '../somedayImage/SomedayImage'
+import ProjectImage from '../projectImage/ProjectImage'
 import './EmptyTaskList.less'
 
 const EmptyTaskList = ({ sectionType = INBOX }) => {
@@ -9,7 +12,37 @@ const EmptyTaskList = ({ sectionType = INBOX }) => {
       return (
         <div className='empty-tasks'>
           <InboxImage />
-          <div className='empty-tasks__text'>
+          <div className='empty-tasks__text' id='inbox-empty-state'>
+            <p className='empty-tasks__text-lead'>Inbox is the best place to capture thoughts, plans, and ideas to classify them later.</p>
+            <p className='empty-tasks__text-basic'>By the way, It's a good idea to clean your inbox at least once a week.</p>
+          </div>
+        </div>
+      )
+    case NEXT:
+      return (
+        <div className='empty-tasks'>
+          <NextImage />
+          <div className='empty-tasks__text' id='next-empty-state'>
+            <p className='empty-tasks__text-lead'>Inbox is the best place to capture thoughts, plans, and ideas to classify them later.</p>
+            <p className='empty-tasks__text-basic'>By the way, It's a good idea to clean your inbox at least once a week.</p>
+          </div>
+        </div>
+      )
+    case SOMEDAY:
+      return (
+        <div className='empty-tasks'>
+          <SomedayImage />
+          <div className='empty-tasks__text' id='someday-empty-state'>
+            <p className='empty-tasks__text-lead'>Inbox is the best place to capture thoughts, plans, and ideas to classify them later.</p>
+            <p className='empty-tasks__text-basic'>By the way, It's a good idea to clean your inbox at least once a week.</p>
+          </div>
+        </div>
+      )
+    case PROJECT:
+      return (
+        <div className='empty-tasks'>
+          <ProjectImage />
+          <div className='empty-tasks__text' id='project-empty-state'>
             <p className='empty-tasks__text-lead'>Inbox is the best place to capture thoughts, plans, and ideas to classify them later.</p>
             <p className='empty-tasks__text-basic'>By the way, It's a good idea to clean your inbox at least once a week.</p>
           </div>
