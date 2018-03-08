8af020921e223524a9f50be960e37f780a478d9a
maksimsemenov <maksa.semenov@gmail.com>
2018-03-07 22:54:56 -0500

    Update tests

--- tests/actions/projectActions_spec.js
+++ tests/actions/projectActions_spec.js
@@ -2,42 +2,47 @@ import * as types from '../../src/constants/actionTypes'
 import * as actions from '../../src/actions/projectActions'
 
 describe('Project action creators', () => {
-    test('Should create an action to add a project', () => {
-      const properties = {
-        title: 'New task',
-        description: 'Description of the task'
-      }
-      expect(actions.addProject(properties)).toEqual(
-          {type: types.ADD_PROJECT, properties: { ...properties, createdDate: Date.now() }}
-      );
-    })
+  test('Should create an action to add a project', () => {
+    const properties = {
+      title: 'New task',
+      description: 'Description of the task'
+    }
+    const action = actions.addProject(properties)
+    expect(action).toMatchObject({ type: types.ADD_PROJECT, properties })
+    expect(action.properties.createdDate).toBeLessThanOrEqual(Date.now())
+  })
 
-    test('Should create an action to remove a project', () => {
-        const id = 0
-        const expectedAction = {
-            type: types.REMOVE_PROJECT,
-            id
-        }
-        expect(actions.removeProject(id)).toEqual(expectedAction);
-    })
+  test('Should create an action to remove a project', () => {
+    const id = 0
+    const expectedAction = {
+      type: types.REMOVE_PROJECT,
+      id
+    }
+    expect(actions.removeProject(id)).toEqual(expectedAction)
+  })
 
-    test('Should create an action to complete a project', () => {
-        const id = 0
-        const status = true
-        expect(actions.completeProject(id, status)).toEqual({ type: types.COMPLETE_PROJECT, date: Date.now(), id, status })
+  test('Should create an action to complete a project', () => {
+    const id = 0
+    const status = true
+    expect(actions.completeProject(id, status)).toEqual({
+      type: types.COMPLETE_PROJECT,
+      date: Date.now(),
+      id,
+      status
     })
+  })
 
-    test('Should create an action to edit a project', () => {
-        const id = 0
-        const properties = {
-            title: 'New task',
-            description: 'Description of the task'
-        }
-        const expectedAction = {
-            type: types.EDIT_PROJECT,
-            id,
-            properties
-        }
-        expect(actions.editProject(id, properties)).toEqual(expectedAction);
-    })
+  test('Should create an action to edit a project', () => {
+    const id = 0
+    const properties = {
+      title: 'New task',
+      description: 'Description of the task'
+    }
+    const expectedAction = {
+      type: types.EDIT_PROJECT,
+      id,
+      properties
+    }
+    expect(actions.editProject(id, properties)).toEqual(expectedAction)
+  })
 })
--- tests/actions/taskActions_spec.js
+++ tests/actions/taskActions_spec.js
@@ -3,101 +3,101 @@ import * as actions from '../../src/actions/taskActions'
 import * as types from '../../src/constants/actionTypes'
 
 describe('Task action creators', () => {
-    test('Should create an action to add a task', () => {
-      const properties = {
-        title: 'New task',
-        description: 'Description of the task'
-      }
-      expect(actions.addTask(properties)).toEqual(
-          { type: types.ADD_TASK, properties: { ...properties, createdDate: Date.now() }}
-      )
-    })
+  test('Should create an action to add a task', () => {
+    const properties = {
+      title: 'New task',
+      description: 'Description of the task'
+    }
+    const action = actions.addTask(properties)
+    expect(action).toMatchObject({ type: types.ADD_TASK, properties })
+    expect(action.properties.createdDate).toBeLessThanOrEqual(Date.now())
+  })
 
-    test('Should create an action to remove a task', () => {
-        const id = 0
-        const expectedAction = {
-            type: types.REMOVE_TASK,
-            id
-        }
-        expect(actions.removeTask(id)).toEqual(expectedAction)
-    })
+  test('Should create an action to remove a task', () => {
+    const id = 0
+    const expectedAction = {
+      type: types.REMOVE_TASK,
+      id
+    }
+    expect(actions.removeTask(id)).toEqual(expectedAction)
+  })
 
-    test('Should create an action to edit a task', () => {
-        const id = 0
-        const properties = {
-            title: 'New task title',
-            description: 'Description of task'
-        }
-        const expectedAction = {
-            type: types.EDIT_TASK,
-            id,
-            properties
-        }
-        expect(actions.editTask(id, properties)).toEqual(expectedAction)
-    })
+  test('Should create an action to edit a task', () => {
+    const id = 0
+    const properties = {
+      title: 'New task title',
+      description: 'Description of task'
+    }
+    const expectedAction = {
+      type: types.EDIT_TASK,
+      id,
+      properties
+    }
+    expect(actions.editTask(id, properties)).toEqual(expectedAction)
+  })
 
-    test('Should create an action to complete a task', () => {
-        const id = 0
-        const status = true
-        let action
-        const dispatch = a => action = a
-        const getState = () => fromJS({})
-        actions.completeTask(id, status)(dispatch, getState)
-        expect(action).toEqual({ type: types.COMPLETE_TASK, date: Date.now(), id, status })
-    })
+  test('Should create an action to complete a task', () => {
+    const id = 0
+    const status = true
+    let action
+    const dispatch = a => (action = a)
+    const getState = () => fromJS({})
+    actions.completeTask(id, status)(dispatch, getState)
+    expect(action).toMatchObject({ type: types.COMPLETE_TASK, id, status })
+    expect(action.date).toBeLessThanOrEqual(Date.now())
+  })
 
-    test('Should create an action to add task to a project', () => {
-        const id = 0
-        const projectId = 0;
-        const expectedAction = {
-            type: types.ADD_TASK_TO_PROJECT,
-            id,
-            project: projectId
-        }
-        expect(actions.addTaskToProject(id, projectId)).toEqual(expectedAction);
-    })
+  test('Should create an action to add task to a project', () => {
+    const id = 0
+    const projectId = 0
+    const expectedAction = {
+      type: types.ADD_TASK_TO_PROJECT,
+      id,
+      project: projectId
+    }
+    expect(actions.addTaskToProject(id, projectId)).toEqual(expectedAction)
+  })
 
-    test('Should create an action to add context to a task', () => {
-        const id = 0
-        const contextId = 0
-        const expectedAction = {
-            type: types.ADD_TASK_CONTEXT,
-            id,
-            context: contextId
-        }
-        expect(actions.addTaskContext(id, contextId)).toEqual(expectedAction)
-    })
+  test('Should create an action to add context to a task', () => {
+    const id = 0
+    const contextId = 0
+    const expectedAction = {
+      type: types.ADD_TASK_CONTEXT,
+      id,
+      context: contextId
+    }
+    expect(actions.addTaskContext(id, contextId)).toEqual(expectedAction)
+  })
 
-    test('Should create an action to remove context from a task', () => {
-        const id = 0
-        const contextId = 0
-        const expectedAction = {
-            type: types.REMOVE_TASK_CONTEXT,
-            id,
-            context: contextId
-        }
-        expect(actions.removeTaskContext(id, contextId)).toEqual(expectedAction)
-    })
-    test('Should create an action to switch task context', () => {
-        const id = 0
-        const contextId = 0
-        const expectedAction = {
-            type: types.SWITCH_TASK_CONTEXT,
-            id,
-            context: contextId
-        }
-        expect(actions.switchTaskContext(id, contextId)).toEqual(expectedAction)
-    })
+  test('Should create an action to remove context from a task', () => {
+    const id = 0
+    const contextId = 0
+    const expectedAction = {
+      type: types.REMOVE_TASK_CONTEXT,
+      id,
+      context: contextId
+    }
+    expect(actions.removeTaskContext(id, contextId)).toEqual(expectedAction)
+  })
+  test('Should create an action to switch task context', () => {
+    const id = 0
+    const contextId = 0
+    const expectedAction = {
+      type: types.SWITCH_TASK_CONTEXT,
+      id,
+      context: contextId
+    }
+    expect(actions.switchTaskContext(id, contextId)).toEqual(expectedAction)
+  })
 
-    test('Should create an action to add task to today', () => {
-        const id = 0
-        const status = true
-        const expectedAction = {
-            type: types.SET_TASK_TODAY,
-            id,
-            status
-        }
-        expect(actions.setTaskToday(id, status)).toEqual(expectedAction)
-    })
-
+  test('Should create an action to add task to today', () => {
+    const id = 0
+    const status = true
+    const expectedAction = {
+      type: types.SET_TASK_TODAY,
+      id,
+      status
+    }
+    expect(actions.setTaskToday(id, status)).toEqual(expectedAction)
+  })
 })
