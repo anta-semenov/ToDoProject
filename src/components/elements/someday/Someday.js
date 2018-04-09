74839445d19fe67a57754600206cac52802a758f
maksimsemenov <maksa.semenov@gmail.com>
2018-04-08 23:36:58 -0400

    Replace Someday Icon

--- src/components/elements/someday/Someday.js
+++ src/components/elements/someday/Someday.js
@@ -1,15 +1,34 @@
-import PropTypes from 'prop-types';
+import PropTypes from 'prop-types'
 import React from 'react'
+import cn from 'classnames'
+import Icon from '../icon/Icon'
 import './Someday.less'
 
-const Someday = ({ appearance = 'default', checked = false, dimmed = false, disabled = false, onClick }) => (
-  <div className={`someday someday--${appearance} ${checked ? 'is-checked' : ''} ${dimmed ? 'is-dimmed' : ''} ${disabled ? 'is-disabled' : ''}`} onClick={() => onClick()} >
-    ...
+const Someday = ({
+  appearance = 'default',
+  checked = false,
+  dimmed = false,
+  disabled = false,
+  onClick
+}) => (
+  <div
+    className={cn(
+      `someday someday--${appearance}`,
+      dimmed && 'is-dimmed',
+      disabled && 'is-disabled'
+    )}
+    onClick={() => onClick()}
+  >
+    <Icon
+      name={checked ? 'someday-dark' : 'someday'}
+      // name="someday"
+      className={cn(`someday__icon someday__icon--${appearance}`, checked && 'is-checked')}
+    />
   </div>
 )
 
 Someday.propTypes = {
-  appearance: PropTypes.oneOf(['task-info', 'tasks-list', 'default']),
+  appearance: PropTypes.oneOf(['task-info', 'default']),
   checked: PropTypes.bool,
   dimmed: PropTypes.bool,
   disabled: PropTypes.bool,
